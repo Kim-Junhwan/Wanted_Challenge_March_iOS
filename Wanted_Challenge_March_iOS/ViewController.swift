344f5e0e1284078f93535e79de9a64dded78de76
Kim-Junhwan <tazanboy1@naver.com>
2023-02-22 20:37:48 +0900

    프로젝트 생성

---
+++ Wanted_Challenge_March_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wanted_Challenge_March_iOS
+//
+//  Created by JunHwan Kim on 2023/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
